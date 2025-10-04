b787a5274b0211d9b1bd9174bb95d31a451a0092
abbasqaziyev2006 <abbasgg@code.edu.az>
2025-10-04 20:56:41 +0400

    edit

--- ECommerce/Controllers/AccountController.cs
+++ ECommerce/Controllers/AccountController.cs
@@ -17,12 +17,16 @@ namespace ECommerce.MVC.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IWishlistItemService _wishlistItemService;
+        private readonly IProductService _productService;
 
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> roleManager)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> roleManager, IWishlistItemService userWishlistItemService, IProductService productService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _roleManager = roleManager;
+            _wishlistItemService = userWishlistItemService;
+            _productService = productService;
         }
 
         [Authorize]
@@ -121,5 +125,92 @@ namespace ECommerce.MVC.Controllers
         {
             return View();
         }
+
+        [Authorize]
+        public async Task<IActionResult> Edit()
+        {
+            var username = User.Identity!.Name ?? "";
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null)
+                return BadRequest();
+
+            var editAccountViewModel = new EditAccountViewModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+
+            };
+
+
+            return View(editAccountViewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditAccountViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+
+            var username = User.Identity!.Name ?? "";
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null)
+                return BadRequest();
+
+
+            if (!string.IsNullOrEmpty(model.CurrentPassword) && !string.IsNullOrEmpty(model.NewPassword))
+            {
+                var resultPassword = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+                if (!resultPassword.Succeeded)
+                {
+                    foreach (var error in resultPassword.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+
+            if (model.Email != user.Email /*&& !string.IsNullOrEmpty(model.Email) && !string.IsNullOrEmpty(user.Email)*/)
+            {
+                var resultEmail = await _userManager.SetEmailAsync(user, model.Email);
+                if (!resultEmail.Succeeded)
+                {
+                    foreach (var error in resultEmail.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+
+                    return View(model);
+                }
+            }
+
+            if (model.FirstName != user.FirstName)
+                user.FirstName = model.FirstName;
+
+            if (model.LastName != user.LastName)
+                user.LastName = model.LastName;
+
+            var resultTotal = await _userManager.UpdateAsync(user);
+
+            if (!resultTotal.Succeeded)
+            {
+                foreach (var error in resultTotal.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
     }
 }
